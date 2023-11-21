e3c295010aaf3e3d42a21fb5d9e1930cc3dbf9fd
aorus <aorus@DESKTOP-PF0OTUT>
2023-11-21 21:05:10 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,13 @@
+﻿Random rnd = new Random();
+Console.WriteLine("kac adet sayi uretilsin");
+int adet = Convert.ToInt32(Console.ReadLine());
+HashSet<int> sonuc = new HashSet<int>();
+
+while (sonuc.Count < adet)
+{
+    sonuc.Add(rnd.Next(1, 101));
+}
+
+Console.WriteLine(String.Join(", ", sonuc));
+
+
